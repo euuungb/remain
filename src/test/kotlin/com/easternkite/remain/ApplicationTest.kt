c4fb057a85f01c8f5f4b938a1ac60a6e10d3be23
easternkite <easternkite7@gmail.com>
2025-01-07 01:57:28 +0900

    Test WorkFlow 추가

--- src/test/kotlin/com/easternkite/remain/ApplicationTest.kt
+++ src/test/kotlin/com/easternkite/remain/ApplicationTest.kt
@@ -1,21 +1,36 @@
 package com.easternkite.remain
 
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.client.request.*
+import io.ktor.client.statement.bodyAsText
 import io.ktor.http.*
+import io.ktor.serialization.kotlinx.json.json
 import io.ktor.server.testing.*
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
 class ApplicationTest {
-
     @Test
-    fun testRoot() = testApplication {
+    fun testTime() = testApplication {
         application {
             module()
         }
-        client.get("/").apply {
+        val client = createClient {
+            install(ContentNegotiation) {
+                json()
+            }
+        }
+        client.post("/time") {
+            contentType(ContentType.Application.Json)
+            setBody(
+                DrRequestBody(
+                    text = "19:00",
+                    command = "/time"
+                )
+            )
+        }.apply {
+            println("response = ${bodyAsText()}")
             assertEquals(HttpStatusCode.OK, status)
         }
     }
-
 }
