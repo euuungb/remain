c5f645d000afd7b824114750d7bc3eea469086b1
Lee Dong-Yeon <easternkite7@gmail.com>
2025-01-07 02:00:51 +0900

    Test WorkFlow 추가 (#8)

--- src/main/kotlin/com/easternkite/remain/Routing.kt
+++ src/main/kotlin/com/easternkite/remain/Routing.kt
@@ -68,16 +68,16 @@ data class DrResponse(
 
 @Serializable
 data class DrRequestBody(
-    val channelId: String,
-    val channelName: String,
-    val tenantId: String,
-    val tenantDomain: String,
-    val command: String,
-    val responseUrl: String,
-    val text: String,
-    val appToken: String,
-    val cmdToken: String,
-    val triggerId: String,
-    val userId: String,
-    val userEmail: String
+    val channelId: String = "",
+    val channelName: String = "",
+    val tenantId: String = "",
+    val tenantDomain: String = "",
+    val command: String = "",
+    val responseUrl: String = "",
+    val text: String = "",
+    val appToken: String = "",
+    val cmdToken: String = "",
+    val triggerId: String = "",
+    val userId: String = "",
+    val userEmail: String = ""
 )
