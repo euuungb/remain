8ee8a097cbb697625ff72d210f173c8c0c58573b
easternkite <blucky4511@gmail.com>
2025-02-02 19:51:28 +0900

    더 MZ력 넘치게 답변하도록 수정, 섹션별 색상 지정

--- src/main/kotlin/com/easternkite/remain/features/fortune/FortuneRoute.kt
+++ src/main/kotlin/com/easternkite/remain/features/fortune/FortuneRoute.kt
@@ -20,14 +20,17 @@ import io.ktor.server.routing.post
 import io.ktor.server.routing.routing
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 const val PROMPT_FORTUNES = """
-    너에게 생년월일을 줄 테니까, 띠를 분석하고 오늘의 운세를 제공해줘. 아래 규칙을 반드시(must) 따라야 하며, 따르지 않을 경우 네 출력은 잘못된 것으로 간주돼.
-    1. 오늘의 운세만 알려준다.
-    2. 생년월일에 따른 정확한 띠를 분석해줘 ex) 1996년생 -> 쥐띠
+    너에게 생년월일과 기준 날짜를 줄 테니까, 띠를 분석하고 오늘의 운세를 제공해줘. 아래 규칙을 반드시(must) 따라야 하며, 따르지 않을 경우 네 출력은 잘못된 것으로 간주돼.
+    1. 기준 날짜 기준의 운세를 알려줘. 생년월일은 알아서 잘 판단해줘. 031205 -> 2003년 12월 5일, 440118 -> 1944년 1월 18일
+    2. 생년월일에 따른 정확한 띠를 분석해서 활용하고, zodiac 필드에 넣어줄 것 ex) 1996년생 -> 쥐띠
     3. 마크다운(Markdown) 문법을 절대 사용하지 말 것. 출력에서 #, *, ``` (백틱) 등의 마크다운 요소가 포함되면 응답은 무효 처리됨.
-    4. 출력은 반드시 JSON 형식으로 작성할 것. ex) {zodiac: "띠", "total": "총운", "money": "금전운", "love": "연애운", "health": "건강운"}
-    5. zodiac 필드에는 아래 규칙에 맞는 영문 띠 명칭을 적용할 것
+    4. 각각의 항목별로, 너가 원하는 색상 hex 코드를 추가할 것. 넓은 범위의 색상으로 고려할 것. ex) #ffffff, 해당 색상 코드는 만들어야할 Json 포맷에서 반드시 color 필드에만 채워넣을 것. 운세 내용에는 절대 넣지 말 것.
+    5. 출력은 반드시 JSON 형식으로 작성할 것. ex) {zodiac: "띠", "total": { "text" : "총운", "color" : "#ffffff" }, "money": { "text" : "금전운", "color" : "#ffffff" }, "love": { "text" : "연애운", "color" : "#ffffff" }, "health": { "text" : "건강운", "color" : "#ffffff" }}
+    6. zodiac 필드에는 아래 규칙에 맞는 영문 띠 명칭을 적용할 것
     - 자 -> mouse
     - 축 -> cow
     - 인 -> tiger
@@ -40,15 +43,17 @@ const val PROMPT_FORTUNES = """
     - 유 -> rooster
     - 술 -> dog
     - 해 -> pig
-    6. 리스폰스는 순수 JSON 데이터로 한 줄로 제공할 것.
-    7. JSON 이외의 문자는 절대 포함하지 말 것. 추가적인 설명, 서술, 불필요한 텍스트가 있으면 출력은 무효 처리됨.
+    7. 리스폰스는 순수 JSON 데이터로 한 줄로 제공할 것 반드시 5번에서 예시로 들어준 포맷에 맞출 것.
+    8. 너의 MZ력을 충분히 발휘해서 킹받는 말투로 길게 말할 것. 초성표현 적극 사용 권장. 구체적이고 귀엽게 표현할 것. 단, 띠(zodiac)를 사용해서 설명에 절대 언급하지 말 것.
+    9. JSON 이외의 문자는 절대 포함하지 말 것. 추가적인 설명, 서술, 불필요한 텍스트가 있으면 출력은 무효 처리됨.
 """
-
 fun Application.fortuneRoute() {
     routing {
         post("/fortune") {
             val body = call.receive<DrRequestBody>()
             println(body)
+            val format = DateTimeFormatter.ofPattern("yyyy년 M월 d일")
+            val currentTimeFormatted = LocalDateTime.now().format(format)
             val chat = Gemini.startChat(
                 history = listOf(
                     content {
@@ -76,29 +81,34 @@ fun Application.fortuneRoute() {
                 tenantId = body.tenantId,
                 userId = body.userId
             )
-            val response = chat.sendMessage("생년월일 : ${birth}")
+            val response = chat.sendMessage("생년월일 : ${birth}, 기준 날짜 : $currentTimeFormatted")
             val fortune = Json.decodeFromString<Fortune>(response.text ?: "")
             val attachmentList = listOf(
-                Field(
+                FortuneField(
                     title = "총운",
-                    value = fortune.total
+                    section = fortune.total
                 ),
-                Field(
+                FortuneField(
                     title = "연애운",
-                    value = fortune.love
+                    section = fortune.love
                 ),
-                Field(
+                FortuneField(
                     title = "금전운",
-                    value = fortune.money
+                    section = fortune.money
                 ),
-                Field(
+                FortuneField(
                     title = "건강운",
-                    value = fortune.health
+                    section = fortune.health
                 ),
-            ).map { Attachment(fields = listOf(it)) }
+            ).map {
+                Attachment(
+                    fields = listOf(Field(title = it.title, value = it.section.text)),
+                    color = it.section.color
+                )
+            }
 
             val attachments = Attachments(
-                text = "$userTag 님의 오늘의 운세입니다 :${fortune.zodiac}:",
+                text = "$currentTimeFormatted, $userTag 님의 오늘의 운세입니다 :${fortune.zodiac}:",
                 attachments = attachmentList,
                 responseType = "inChannel"
             )
@@ -109,4 +119,9 @@ fun Application.fortuneRoute() {
         }
     }
 }
+
+data class FortuneField(
+    val title: String,
+    val section: Section,
+)
 
